5973c6405b9fb39b49279e8737aaa4fe7cfebec8
Yarogi <yachmenyov.igor@yandex.ru>
2024-08-15 10:21:36 +0300

    1. Добавлен Main-экран

---
+++ app/src/main/java/com/practicum/mymovies/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.practicum.mymovies.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.practicum.mymovies.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
