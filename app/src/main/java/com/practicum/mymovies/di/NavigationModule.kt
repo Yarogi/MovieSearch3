af1ac0e99e896917ea259fed7a228f7f0bd4e3e8
Yarogi <yachmenyov.igor@yandex.ru>
2024-08-15 19:41:47 +0300

    Косметика

--- app/src/main/java/com/practicum/mymovies/di/NavigationModule.kt
+++ app/src/main/java/com/practicum/mymovies/di/NavigationModule.kt
@@ -1,5 +1,6 @@
 package com.practicum.mymovies.di
 
+import com.practicum.mymovies.core.navigation.NavigatorHolder
 import com.practicum.mymovies.core.navigation.Router
 import com.practicum.mymovies.core.navigation.impl.RouterImpl
 import org.koin.dsl.module
@@ -8,7 +9,7 @@ val navigationModule = module {
 
     val router = RouterImpl()
 
-    single<Router> {router}
-    single { router.navigatorHolder }
+    single<Router> { router }
+    single<NavigatorHolder> { router.navigatorHolder }
 
 }
