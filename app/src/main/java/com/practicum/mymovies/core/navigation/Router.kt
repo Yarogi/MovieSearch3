6ff297116e0bfc521a90dff6d6e12883e09917f3
Yarogi <yachmenyov.igor@yandex.ru>
2024-08-15 18:42:34 +0300

    1. Router
    Router — это абстракция для Fragment, которые хотят осуществить переход на следующий экран.

---
+++ app/src/main/java/com/practicum/mymovies/core/navigation/Router.kt
@@ -0,0 +1,16 @@
+package com.practicum.mymovies.core.navigation
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Router — это входная точка для фрагментов,
+ * которые хотят открыть следующий экран.
+ */
+
+interface Router {
+
+    val navigationHolder: NavigationHolder
+
+    fun openFragment(fragment:Fragment)
+
+}
