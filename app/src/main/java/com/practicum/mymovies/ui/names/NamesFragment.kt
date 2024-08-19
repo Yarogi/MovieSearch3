c0d2710105103468640b07c569327c5a902a3638
Yarogi <yachmenyov.igor@yandex.ru>
2024-08-19 17:48:19 +0300

    Черновик для навигации

--- app/src/main/java/com/practicum/mymovies/ui/names/NamesFragment.kt
+++ app/src/main/java/com/practicum/mymovies/ui/names/NamesFragment.kt
@@ -1,6 +1,23 @@
 package com.practicum.mymovies.ui.names
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.practicum.mymovies.databinding.FragmentNamesBinding
 
 class NamesFragment : Fragment() {
+
+    private lateinit var binding: FragmentNamesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentNamesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
 }
