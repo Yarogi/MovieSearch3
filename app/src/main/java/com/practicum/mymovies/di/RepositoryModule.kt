ec30775df771d3f520d974f0f1d6c6b45febf0f9
Yarogi <yachmenyov.igor@yandex.ru>
2024-09-13 10:28:39 +0300

    Подключаем зависимости

--- app/src/main/java/com/practicum/mymovies/di/RepositoryModule.kt
+++ app/src/main/java/com/practicum/mymovies/di/RepositoryModule.kt
@@ -4,6 +4,7 @@ import com.practicum.mymovies.data.MoviesRepositoryImpl
 import com.practicum.mymovies.data.NamesRepositoryImpl
 import com.practicum.mymovies.data.SearchHistoryRepositoryImpl
 import com.practicum.mymovies.data.converters.MovieCastConverter
+import com.practicum.mymovies.data.converters.MovieDbConverter
 import com.practicum.mymovies.data.converters.NamesToPersonConverter
 import com.practicum.mymovies.domain.api.MoviesRepository
 import com.practicum.mymovies.domain.api.NamesRepository
@@ -15,6 +16,8 @@ val repositoryModule = module {
     factory { MovieCastConverter() }
 
     factory { NamesToPersonConverter() }
+
+    factory { MovieDbConverter() }
 
     single<MoviesRepository> {
         MoviesRepositoryImpl(
